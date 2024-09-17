56cde613daced9dcf2cc42d831605e605211808b
jessekerch <jesse.kercheval@gmail.com>
2024-09-17 16:41:22 -0700

    endpointServices not using pg yet

--- services/endpointServices.js
+++ services/endpointServices.js
@@ -4,20 +4,20 @@
 // const deleteAllRequests(endpoint_id);
 // const deleteRequestById(endpoint_id, request_id);
 
-const { Client } = require("pg");
+// const { Client } = require("pg");
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// // const client = new Client({
+// //   user: process.env.DB_USER,
+// //   host: process.env.DB_HOST,
+// //   database: process.env.DB_NAME,
+// //   password: process.env.DB_PASSWORD,
+// //   port: process.env.DB_PORT,
+// // });
 
-client
-  .connect()
-  .then(() => console.log("Connected to PostgreSQL"))
-  .catch((err) => console.error(err, "Error Connection to PostgreSQL"));
+// client
+//   .connect()
+//   .then(() => console.log("Connected to PostgreSQL"))
+//   .catch((err) => console.error(err, "Error Connection to PostgreSQL"));
 
 function generateRandomHash() {
   const hash_length = 8;
