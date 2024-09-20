6da9cd2ac583307caf43019bc5845a810acbe382
Wook Kim <h.wook.kim@gmail.com>
2024-09-19 17:52:26 -0700

    change static routing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('dist'));
+app.use("/", express.static('dist'));
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
